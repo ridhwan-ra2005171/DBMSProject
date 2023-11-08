edfa789c436bd315e0ebf39c557b68f4ada3f6e9
Mohamad Allaham <ma2005872@qu.edu.qa>
2023-11-08 17:56:54 +0300

    added "unique" column to the Column Metadata Table in the Metadata Doc

--- DBMS_QueryCostApp/app/src/main/java/com/test/querycostapp/algorithms/searchAlgorithms.kt
+++ DBMS_QueryCostApp/app/src/main/java/com/test/querycostapp/algorithms/searchAlgorithms.kt
@@ -1,2 +1,9 @@
 package com.test.querycostapp.algorithms
 
+// S1 - Linear Search (brute force) approach
+fun linearSearch() : Int {
+    // If the record is not in the table, then search the whole table
+    // meaning search all the blocks
+    // otherwise the cost is searching half of the blocks
+
+}
